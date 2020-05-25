f9c1407071c960a8cddf1aa5c7cc14e6156bc4c9
Luke Prosser <lukesprosser@gmail.com>
2020-05-25 18:58:57 +0100

    Blog index | Home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Celtic Kitchens`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Amanda Price`,
+      summary: `- Marketing Manager`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://celtickitchens.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `createdeluxe`,
     },
   },
   plugins: [
