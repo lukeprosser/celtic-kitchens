9dabf104ed7b61c7d9587fd9b67ec4c6b3ed152e
Luke Prosser <lukesprosser@gmail.com>
2020-05-26 17:08:01 +0100

    Typography theme | Header component

---
+++ src/components/header.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "./header.scss"
+
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <div className="header-mobile">
+        <h1 className="header-brand">{title}</h1>
+        <nav className="header-mobile-nav">
+          <Link className="header-nav-link" to="/portfolio">
+            Portfolio
+          </Link>
+          <Link className="header-nav-link" to="/process">
+            Process
+          </Link>
+          <Link className="header-nav-link" to="/blog">
+            Blog
+          </Link>
+          <Link className="header-nav-link" to="/contact">
+            Contact
+          </Link>
+        </nav>
+        <span className="header-mobile-menu"></span>
+      </div>
+      <div className="header-desktop">
+        <Link className="header-nav-link" to="/portfolio">
+          Portfolio
+        </Link>
+        <Link className="header-nav-link" to="/process">
+          Process
+        </Link>
+        <h1 className="header-brand">{title}</h1>
+        <Link className="header-nav-link" to="/blog">
+          Blog
+        </Link>
+        <Link className="header-nav-link" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "./header"
 
 import { rhythm, scale } from "../utils/typography"
+import "./layout.scss"
 
 const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
   return (
     <div
       style={{
@@ -56,12 +15,12 @@ const Layout = ({ location, title, children }) => {
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
-      <header>{header}</header>
+      <Header title={title} />
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        © Celtic Kitchens {new Date().getFullYear()}
+        {` `} | Built by {` `}
+        <a href="https://www.createdeluxe.com">CreateDeluxe</a>
       </footer>
     </div>
   )
