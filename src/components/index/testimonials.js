babd0ac15afad26a6193ef2d0a318660f523bc90
Luke Prosser <lukesprosser@gmail.com>
2020-05-29 11:14:53 +0100

    Testimonials style

--- src/components/index/testimonials.js
+++ src/components/index/testimonials.js
@@ -9,24 +9,26 @@ const Testimonials = () => {
         <h2 className="page-section-header">What our customers say</h2>
         <div className="testimonials-content">
           <div className="testimonials-content-item">
-            <p>
+            <p className="testimonials-content-item-quote">
               "Outstanding quality, I couldn't believe the final result. Thank
               you so much to David and the team!""
             </p>
-            <p>- Barbara Smith</p>
+            <p className="testimonials-content-item-customer">
+              - Barbara Smith
+            </p>
           </div>
           <div className="testimonials-content-item">
-            <p>
+            <p className="testimonials-content-item-quote">
               "Who knew a kitchen would completely transform our home? Really
               inspiring design and top quality!"
             </p>
-            <p>- Kevin James</p>
+            <p className="testimonials-content-item-customer">- Kevin James</p>
           </div>
           <div className="testimonials-content-item">
-            <p>
+            <p className="testimonials-content-item-quote">
               "Great communication throughout the process, highly recommended."
             </p>
-            <p>- Steve Owen</p>
+            <p className="testimonials-content-item-customer">- Steve Owen</p>
           </div>
         </div>
       </div>
